3ca5782563b9cee424b9a41d7abe7ac73b103080
Eleanor Wright <elliewright055@gmail.com>
2025-06-20 07:28:31 -0500

    Create backend using Spring Initializr

---
+++ backend/src/main/java/com/ellewright/villagr/VillagrApplication.java
@@ -0,0 +1,13 @@
+package com.ellewright.villagr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillagrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VillagrApplication.class, args);
+	}
+
+}
