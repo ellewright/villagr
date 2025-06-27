d4b5ea2e118c60b5003bf9e21958f5c91841477f
Eleanor Wright <elliewright055@gmail.com>
2025-06-27 17:00:11 -0500

    Create Villager entity class

---
+++ backend/src/main/java/com/ellewright/villagr/entity/Villager.java
@@ -0,0 +1,20 @@
+package com.ellewright.villagr.entity;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Document(collection = "villagers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Villager {
+    @Id
+    private ObjectId id;
+    private String name;
+    private String job;
+}
