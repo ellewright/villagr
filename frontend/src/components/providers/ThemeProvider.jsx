f3eb9e3e2780fc53d45fa328d42c409cdcfad861
Eleanor Wright <elliewright055@gmail.com>
2025-08-09 17:20:48 -0500

    Persist theme to localStorage

--- frontend/src/components/providers/ThemeProvider.jsx
+++ frontend/src/components/providers/ThemeProvider.jsx
@@ -2,13 +2,23 @@ import { useEffect, useState } from "react";
 import { ThemeContext } from "../../contexts/ThemeContext";
 
 export default function ThemeProvider({ children }) {
-    const [isDarkMode, setIsDarkMode] = useState(true);
+    const [isDarkMode, setIsDarkMode] = useState(() => {
+        const initialValue = localStorage.getItem("DARK_MODE");
+
+        if (initialValue !== null) {
+            return JSON.parse(initialValue);
+        } else {
+            return true;
+        }
+    });
 
     function toggleTheme() {
-        setIsDarkMode(d => !d);
+        setIsDarkMode(prev => !prev);
     }
 
     useEffect(() => {
+        localStorage.setItem("DARK_MODE", JSON.stringify(isDarkMode));
+
         if (isDarkMode) {
             document.body.style.backgroundColor = "#111";
             document.body.style.color = "#999";
