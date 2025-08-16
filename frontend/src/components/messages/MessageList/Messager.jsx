67130248c1932ecd7181eaa465301cfd89bec48e
Eleanor Wright <elliewright055@gmail.com>
2025-08-16 12:43:47 -0500

    Create Message sending functionality

--- frontend/src/components/messages/MessageList/Messager.jsx
+++ frontend/src/components/messages/MessageList/Messager.jsx
@@ -1,39 +1,76 @@
+import { useState } from "react";
 import styles from "./Messager.module.css";
 
 export default function Messager({ villager }) {
+    const [newMessage, setNewMessage] = useState("");
+    const [messages, setMessages] = useState([
+        {
+            id: crypto.randomUUID(),
+            author: villager.name,
+            message: "Hello! How can I help you today?"
+        }
+    ]);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        setMessages((prev) => [...prev, {
+            id: crypto.randomUUID(),
+            author: "User",
+            message: newMessage
+        }]);
+
+        setNewMessage("");
+    }
+
     return (
         <div className={styles.chatContainer}>
             <div className={styles.chatList}>
-                <div className={`${styles.chat} ${styles.user}`}>
-                    <p className={`${styles.chatMessage} ${styles.user}`}>
-                        Hello!
-                    </p>
-                    <img
-                        className={styles.chatImg}
-                        src="/User.jpg"
-                        alt="User profile picture."
-                    />
-                </div>
-                <div className={`${styles.chat} ${styles.villager}`}>
-                    <img
-                        className={styles.chatImg}
-                        src={`/${villager.name}.png`}
-                        alt="Villager profile picture."
-                    />
-                    <p className={`${styles.chatMessage} ${styles.villager}`}>
-                        Hello! How can I help you today?
-                    </p>
-                </div>
+                {messages.map((message) => (
+                    <div
+                        key={message.id}
+                        className={`${styles.chat} 
+                        ${message.author === "User"
+                                ? styles.user
+                                : styles.villager}`}
+                    >
+                        {message.author !== "User" && (
+                            <img
+                                className={styles.chatImg}
+                                src={`/${villager.name}.png`}
+                                alt="User profile picture."
+                            />
+                        )}
+                        <p className={`${styles.chatMessage} 
+                            ${message.author === "User"
+                                ? styles.user
+                                : styles.villager}`}
+                        >
+                            {message.message}
+                        </p>
+                        {message.author === "User" && (
+                            <img
+                                className={styles.chatImg}
+                                src="/User.jpg"
+                                alt="User profile picture."
+                            />
+                        )}
+                    </div>
+                ))}
             </div>
-            <form className={styles.newChatForm}>
+            <form
+                className={styles.newChatForm}
+                onSubmit={handleSubmit}
+            >
                 <input
                     className={styles.newChatInput}
-                    type="text"
+                    value={newMessage}
+                    onChange={(e) => setNewMessage(e.target.value)}
                 />
                 <button className={styles.newChatButton}>
                     Send
                 </button>
             </form>
         </div>
-    )
+    );
 }
